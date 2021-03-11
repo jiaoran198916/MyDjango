6498fb14a6317e9a95a1f1d9ba52c9c03053757e
jiaoran <920495391@qq.com>
2021-03-11 11:35:59 +0800

    设置域名访问

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'a*l+pv%c&f9ikr6cb!&ay-%3bzb@@t#s!d7%_f*x($+-%#cc&s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.jiaoran.net']
 
 
 # Application definition
