89b8e75700673810b802c89d396e6595baa3931c
jiaoran <920495391@qq.com>
2021-03-13 16:33:24 +0800

    strip——blank

--- blog/admin.py
+++ blog/admin.py
@@ -9,9 +9,9 @@ class PostAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         obj.author = request.user
         super().save_model(request, obj, form, change)
+
+
 # Register your models here.
-
-
 admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
 admin.site.register(Tag)
